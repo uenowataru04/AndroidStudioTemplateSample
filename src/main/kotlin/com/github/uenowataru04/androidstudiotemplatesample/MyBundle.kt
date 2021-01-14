8202a45cf24f558ff9c9449f020dae78e08d5122
GitHub Action <action@github.com>
2021-01-14 17:20:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/uenowataru04/androidstudiotemplatesample/MyBundle.kt
+++ src/main/kotlin/com/github/uenowataru04/androidstudiotemplatesample/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.uenowataru04.androidstudiotemplatesample
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/uenowataru04/androidstudiotemplatesample/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.uenowataru04.androidstudiotemplatesample.services
+
+import com.github.uenowataru04.androidstudiotemplatesample.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
